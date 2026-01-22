85e161ad7284afa631ecee9fec78687bc86fcd8f
Abhay Sorout <you@example.com>
2026-01-22 20:49:45 +0530

    [Abhay Sorout]Add. Positive Negative and Zero Numbers

---
+++ basic-programs/PositiveNegativeZeroNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class PositiveNegativeZeroNumber{
+	public static void main(String[] args){	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the number:");
+	int a = sc.nextInt();  
+	if(a == 0) {
+		System.out.println("Zero");
+		}
+	else if(a > 0) {
+		System.out.println("Positive");
+		}
+	else if(a < 0) {
+		System.out.println("Negative");
+		}
+	}
+	}
