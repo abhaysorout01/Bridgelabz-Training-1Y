4da307facbff9c78ff6d46c3d237ad7127a7bdfd
Abhay Sorout <you@example.com>
2026-03-29 10:04:07 +0530

    [Abhay Sorout]Changed a file

--- src/com/gla/StoryBasedQuestions/Main.java
+++ src/com/gla/StoryBasedQuestions/Main.java
@@ -1,4 +1,28 @@
-package com.gla.StoryBasedQuestions;
+package com.gla.string;
+class MyThread extends Thread{
+    @Override
+    public void run() {
+        for(int i = 0;i < 10;i++) {
+            System.out.println(i+1);
+        }
+    }
+}
+class OtherThread extends Thread{
+    @Override
+    public void run() {
+        for(int i = 0;i < 10;i++) {
+            System.out.println((char) (i + 'a'));
+        }
+    }
+}
 
 public class Main {
+    static void main() {
+        System.out.println("main");
+        MyThread mt = new MyThread();
+        OtherThread ot = new OtherThread();
+        mt.run();
+        ot.run();
+        System.out.println("main ending");
+    }
 }
